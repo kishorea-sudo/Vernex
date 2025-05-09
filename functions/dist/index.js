c57bb8719d46b3ee2d2472c38bd86c2e481d3b27
Kishore <kishorea2806@gmail.com>
2025-05-09 16:25:07 +0530

    changes

---
+++ functions/dist/index.js
@@ -0,0 +1,54 @@
+// server/index.ts
+import express2 from "express";
+
+// server/routes.ts
+import express from "express";
+var registerRoutes = async (app2) => {
+  const server = express.Router();
+  app2.use("/api", server);
+  return app2.listen({ port: 0, host: "0.0.0.0", reusePort: true });
+};
+
+// server/index.ts
+var app = express2();
+app.use(express2.json());
+app.use(express2.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  const start = Date.now();
+  const path = req.path;
+  let capturedJsonResponse = void 0;
+  const originalResJson = res.json;
+  res.json = function(bodyJson, ...args) {
+    capturedJsonResponse = bodyJson;
+    return originalResJson.apply(res, [bodyJson, ...args]);
+  };
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    if (path.startsWith("/api")) {
+      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+      if (capturedJsonResponse) {
+        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+      }
+      if (logLine.length > 80) {
+        logLine = logLine.slice(0, 79) + "\u2026";
+      }
+      log(logLine);
+    }
+  });
+  next();
+});
+(async () => {
+  const server = await registerRoutes(app);
+  app.use((err, _req, res, _next) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(status).json({ message });
+    throw err;
+  });
+  if (app.get("env") === "development") {
+  }
+})();
+var index_default = app;
+export {
+  index_default as default
+};
